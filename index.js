9bd702b6bf54f8fd5d5703f217de30389eae7cf2
benjir bhuyan <benjir_bhuyan@yahoo.com>
2024-07-21 11:50:39 +0800

    added user post and get methods

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ async function run() {
         await client.connect();
 
         const woodCollection = client.db("crufts").collection("allcrufts");
+        const cruftUsers = client.db("crufts").collection("cruftusers");
         // Send a ping to confirm a successful connection
 
         app.get('/addcruft', async(req, res)=>{
@@ -42,12 +43,30 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
-        
+        app.get('/addcruft/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await woodCollection.findOne(query);
+            res.send(result)
+        })
         app.post('/addcruft', async (req, res) => {
             const newcruft = req.body;
             console.log(newcruft);
             const result =await woodCollection.insertOne(newcruft);
             res.send(result);
+        })
+
+        //create users
+        app.post('/users', async(req, res)=>{
+            const newuser = req.body;
+            const result =await cruftUsers.insertOne(newuser);
+            res.send(result)
+        })
+        app.get('/users', async(req, res)=>{
+            const cursor= cruftUsers.find();
+            const result = await cursor.toArray();
+            res.send(result)
+
         })
 
         await client.db("admin").command({ ping: 1 });
